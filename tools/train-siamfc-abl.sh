f8658c4554a445781dc6b7434edd3daa8c787ce0
Jorrit Ypenga <jorrit.ypenga@student.uva.nl>
2020-01-06 21:48:44 +0100

    ablation addition

--- tools/train-siamfc-abl.sh
+++ tools/train-siamfc-abl.sh
@@ -8,4 +8,4 @@
 module load cuda90
 
 export PATH="/var/scratch/jypenga/anaconda3/bin:$PATH"
-python train.py --model="siamfc" --save="/var/scratch/jypenga/temp" --root="/var/scratch/jypenga/data" --batch_size=64 --num_workers=16 --seq_len=2 --epoch_n=50 --subset='train' --ablation=1
+python train.py --model="siamfc" --save="/var/scratch/jypenga/temp" --root="/var/scratch/jypenga/data/GOT-10k" --batch_size=64 --num_workers=16 --seq_len=2 --epoch_n=50 --subset='train' --ablation=1
